53413adb7a9dbf1cba25155fbf9a34a7745edad9
0xdeadbeefjerky <dominic.spinosa@gmail.com>
2022-10-02 14:58:23 -0400

    format all Terraform files

--- terraform/examples/gcp.tf
+++ terraform/examples/gcp.tf
@@ -5,13 +5,13 @@ module "ssh_key" {
 }
 
 module "gcp" {
-  source = "./modules/gcp"
+  source             = "./modules/gcp"
   public_key_openssh = module.ssh_key.public_key_openssh
-  ports = var.ports
-  privkey_filename = module.ssh_key.privkey_filename
-  project = "mythic-deploy-test"
-  region = "us-east1"
-  zone = "us-east1-b"
+  ports              = var.ports
+  privkey_filename   = module.ssh_key.privkey_filename
+  project            = "mythic-deploy-test"
+  region             = "us-east1"
+  zone               = "us-east1-b"
 }
 
 output "vm_ip" {
--- terraform/modules/gcp/outputs.tf
+++ terraform/modules/gcp/outputs.tf
@@ -1,14 +1,14 @@
 output "vm_ip" {
-  value = google_compute_instance.main.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.main.network_interface[0].access_config[0].nat_ip
   description = "Public IP for Mythic VM instance"
 }
 
 output "hostname" {
-  value = var.name
+  value       = var.name
   description = "Hostname assigned to server for SSH connections"
 }
 
 output "ssh_config" {
-  value = local_file.sshconfig.filename
+  value       = local_file.sshconfig.filename
   description = "Full path to SSH config file"
 }
